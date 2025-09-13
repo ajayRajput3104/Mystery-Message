057879c617256edfe2d27a62bc90e24ce92a53e1
ajayRajput3104 <2004ajrajput@gmail.com>
2025-09-13 17:08:47 +0530

    apos error solved

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -120,7 +120,7 @@ function SignInComponent() {
         </Form>
         <div className="text-center mt-4">
           <p>
-            Don't Have an Account?{" "}
+            Don&apos;t Have an Account?{" "}
             <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
               Sign Up
             </Link>
